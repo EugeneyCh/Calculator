55e4114c015642ce9c5edd62818d842b8f352b69
EugeneyCh <eugeney@ua.fm>
2023-11-02 13:43:13 +0200

    Create_App

---
+++ calculator/src/App.js
@@ -0,0 +1,12 @@
+import css from "./App.module.css";
+function App() {
+  return (
+    <div className={css.app}>
+      <header className="App-header">
+        <title>Calc</title>
+      </header>
+    </div>
+  );
+}
+
+export default App;
