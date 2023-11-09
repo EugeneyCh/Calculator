b829904b683e3a427d5f861e26f3c917a2d17c53
EugeneyCh <eugeney@ua.fm>
2023-11-09 14:39:14 +0200

    Eighth-Day-II

--- src/components/NumPad/NumPad.js
+++ src/components/NumPad/NumPad.js
@@ -1,58 +1,58 @@
-import React, { lazy, useEffect, useState } from "react";
-import css from "./NumPad.module.css";
+import React, { useEffect, useState } from 'react';
+import css from './NumPad.module.css';
 // import Button from "../Button/Button";
 
 const NumPad = () => {
-  const [selectedButton, setSelectedButton] = useState("");
-  const [result, setResult] = useState("");
-  const [firstAction, setFirstAction] = useState("");
-  const [firstNumber, setFirstNumber] = useState("");
+  const [selectedButton, setSelectedButton] = useState('');
+  const [result, setResult] = useState('');
+  const [firstAction, setFirstAction] = useState('');
+  const [firstNumber, setFirstNumber] = useState('');
   const [isFirstNumber, setIsFirstNumber] = useState(false);
-  const [lastNumber, setLastNumber] = useState("");
+  const [lastNumber, setLastNumber] = useState('');
   const [isDote, setIsDote] = useState(false);
   const buttonsValues = [
-    "7",
-    "8",
-    "9",
-    "DEL",
-    "4",
-    "5",
-    "6",
-    "+",
-    "1",
-    "2",
-    "3",
-    "-",
-    ",",
-    "0",
-    "/",
-    "x",
-    "RESET",
-    "=",
+    '7',
+    '8',
+    '9',
+    'DEL',
+    '4',
+    '5',
+    '6',
+    '+',
+    '1',
+    '2',
+    '3',
+    '-',
+    ',',
+    '0',
+    '/',
+    'x',
+    'RESET',
+    '=',
   ];
   const checkButton = (buttonsName) => {
-    if (buttonsName === "-" && lastNumber.length === 0) {
-      createNumber("-");
+    if (buttonsName === '-' && lastNumber.length === 0) {
+      createNumber('-');
     } else {
-      if (lastNumber === "-") {
+      if (lastNumber === '-') {
         deleteSign();
       }
     }
     switch (buttonsName) {
-      case "RESET":
+      case 'RESET':
         return reset();
-      case "DEL":
+      case 'DEL':
         return deleteSign();
-      case "=":
+      case '=':
         return actionWithNumbers();
-      case "+":
-        return createAction("+");
-      case "-":
-        return createAction("-");
-      case "x":
-        return createAction("*");
-      case "/":
-        return createAction("/");
+      case '+':
+        return createAction('+');
+      case '-':
+        return createAction('-');
+      case 'x':
+        return createAction('*');
+      case '/':
+        return createAction('/');
       default:
         createNumber(buttonsName);
         break;
@@ -63,11 +63,11 @@ const NumPad = () => {
     checkButton(sign);
   };
   const createNumber = (num) => {
-    isFirstNumber && setLastNumber("");
-    if (num === ",") {
+    isFirstNumber && setLastNumber('');
+    if (num === ',') {
       setIsDote(true);
     }
-    if (isDote && num === ",") {
+    if (isDote && num === ',') {
       return;
     }
     // let numberFull = lastNumber + num;
@@ -77,24 +77,24 @@ const NumPad = () => {
   ////////   RESET    /////////////////////
 
   const reset = () => {
-    setSelectedButton("");
-    setResult("");
-    setLastNumber("");
+    setSelectedButton('');
+    setResult('');
+    setLastNumber('');
     setIsDote(false);
-    setFirstAction("");
-    setFirstNumber("");
+    setFirstAction('');
+    setFirstNumber('');
     setIsFirstNumber(false);
   };
 
   ///////    DELETE       ////////////////////
   const deleteSign = () => {
-    if (lastNumber[lastNumber.length - 1] === ",") {
+    if (lastNumber[lastNumber.length - 1] === ',') {
       setIsDote(false);
     }
     if (lastNumber.length !== 0) {
       setLastNumber(lastNumber.slice(0, -1));
-      setFirstNumber("");
-      setFirstAction("");
+      setFirstNumber('');
+      setFirstAction('');
     }
   };
 
@@ -102,16 +102,16 @@ const NumPad = () => {
   //  ??  firstAction.length>0&&isFirstNumber?actionWithNumbers():
 
   function getResult(f, a, l) {
-    const stringToNumber = (n) => (n + "").split(",").join(".") * 1;
-    const toString = (num) => (num + "").split(".").join(",");
+    const stringToNumber = (n) => (n + '').split(',').join('.') * 1;
+    const toString = (num) => (num + '').split('.').join(',');
     switch (a) {
-      case "+":
+      case '+':
         return toString(stringToNumber(f) + stringToNumber(l));
-      case "-":
+      case '-':
         return toString(stringToNumber(f) - stringToNumber(l));
-      case "*":
+      case '*':
         return toString(stringToNumber(f) * stringToNumber(l));
-      case "/":
+      case '/':
         return toString(stringToNumber(f) / stringToNumber(l));
       default:
         return;
@@ -121,22 +121,22 @@ const NumPad = () => {
   const createAction = (num) => {
     if (firstNumber.length === 0 && lastNumber.length === 0) return;
     if (firstNumber.length > 0 && lastNumber.length > 0) {
-      let res = getResult(firstNumber, firstAction, lastNumber);
+      const res = getResult(firstNumber, firstAction, lastNumber);
       setResult(() => res);
 
       return;
     }
     setFirstAction(num);
-    if (result === "" && firstNumber === "" && lastNumber.length > 0) {
+    if (result === '' && firstNumber === '' && lastNumber.length > 0) {
       // setResult(lastNumber);
       setFirstNumber(lastNumber);
       setIsFirstNumber(true);
-      setLastNumber("");
+      setLastNumber('');
       setIsDote(false);
     }
   };
   const actionWithNumbers = () => {
-    let intResult = +result;
+    const intResult = +result;
     setResult();
   };
   // const sum = () => {
@@ -147,13 +147,13 @@ const NumPad = () => {
     // checkButton(selectedButton);
     // console.log(selectedButton);
     console.log(firstAction);
-    console.log("Last sign is...", lastNumber[lastNumber.length - 1]);
+    console.log('Last sign is...', lastNumber[lastNumber.length - 1]);
     console.log(isDote);
     console.log(lastNumber);
-    console.log("Action", firstAction);
-    console.log("FirstNumber", firstNumber);
-    console.log("LastNumber", lastNumber);
-    console.log("Result is", result);
+    console.log('Action', firstAction);
+    console.log('FirstNumber', firstNumber);
+    console.log('LastNumber', lastNumber);
+    console.log('Result is', result);
   }, [lastNumber, firstAction]);
 
   return (
